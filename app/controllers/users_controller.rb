2d98f72afa20564e21fc8d67851e5e97f2dbf905
TanveetGill <tanveet.gill@gmail.com>
2016-07-18 19:06:09 -0400

    created a User Model and Signup View

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      puts 'success'
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
